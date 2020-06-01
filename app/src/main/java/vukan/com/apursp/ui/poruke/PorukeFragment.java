44fb6d7506ea0dd0188b96a12b9ead226c8e3068
Vukan-Markovic <vukan.markovic97@gmail.com>
2020-06-01 11:55:33 +0200

    fixed navigation

--- app/src/main/java/vukan/com/apursp/ui/poruke/PorukeFragment.java
+++ app/src/main/java/vukan/com/apursp/ui/poruke/PorukeFragment.java
@@ -9,23 +9,30 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
 import vukan.com.apursp.R;
 
 public class PorukeFragment extends Fragment {
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         PorukeViewModel porukeViewModel = ViewModelProviders.of(this).get(PorukeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_poruke, container, false);
         final TextView textView = root.findViewById(R.id.text_poruke);
-        porukeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
+
+        porukeViewModel.getText().observe(getViewLifecycleOwner(), s -> {
+//            textView.setText(s);
         });
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView textView = view.findViewById(R.id.text_poruke);
+        if(getArguments() != null)
+        textView.setText(PorukeFragmentArgs.fromBundle(getArguments()).getId() + "");
     }
 }
