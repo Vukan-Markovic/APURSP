8c2f366800f95baef0cf01270ba09a02bde86d2e
katicm <markokatic1997@gmail.com>
2020-06-18 17:43:10 +0200

    user page

--- app/src/main/java/vukan/com/apursp/ui/mojioglasi/MojioglasiFragment.java
+++ app/src/main/java/vukan/com/apursp/ui/mojioglasi/MojioglasiFragment.java
@@ -9,23 +9,66 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-import vukan.com.apursp.R;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MojioglasiFragment extends Fragment {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+
+import vukan.com.apursp.R;
+import vukan.com.apursp.adapters.ProductRecyclerViewAdapter;
+
+public class MojioglasiFragment extends Fragment implements ProductRecyclerViewAdapter.ListItemClickListener {
+
+    private String userID="0";
+    private TextView username;
+    private TextView location;
+    private TextView phone;
+
+    private ProductRecyclerViewAdapter adapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        MojioglasiViewModel mojioglasiViewModel = ViewModelProviders.of(this).get(MojioglasiViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_mojioglasi, container, false);
-        final TextView textView = root.findViewById(R.id.text_mojioglasi);
-        mojioglasiViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
+        return inflater.inflate(R.layout.fragment_mojioglasi, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        MojioglasiViewModel mojioglasiViewModel = new ViewModelProvider(this).get(MojioglasiViewModel.class);
+
+        RecyclerView.LayoutManager layoutManager=new GridLayoutManager(getContext(),2);
+        RecyclerView recyclerView=view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new ProductRecyclerViewAdapter(new ArrayList<>(), this);
+        recyclerView.setAdapter(adapter);
+
+        FirebaseUser fire_user= FirebaseAuth.getInstance().getCurrentUser();
+
+
+
+        username=view.findViewById(R.id.username);
+        location=view.findViewById(R.id.location);
+        phone=view.findViewById(R.id.phone);
+
+        mojioglasiViewModel.getUser().observe(getViewLifecycleOwner(),user -> {
+            username.setText(user.getUsername());
+            location.setText(user.getLocation());
+            phone.setText(user.getPhone());
+            userID=user.getUserID();
         });
-        return root;
+
+        mojioglasiViewModel.getUserProducts(fire_user.getUid()).observe(getViewLifecycleOwner(), products -> {
+            adapter=new ProductRecyclerViewAdapter(products,this);
+            recyclerView.setAdapter(adapter);
+        });
+    }
+
+    @Override
+    public void onListItemClick(String productID) {
     }
 }
--- app/src/main/java/vukan/com/apursp/ui/mojioglasi/MojioglasiViewModel.java
+++ app/src/main/java/vukan/com/apursp/ui/mojioglasi/MojioglasiViewModel.java
@@ -1,19 +1,33 @@
 package vukan.com.apursp.ui.mojioglasi;
 
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class MojioglasiViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+import java.util.List;
+
+import vukan.com.apursp.models.Product;
+import vukan.com.apursp.models.User;
+import vukan.com.apursp.repository.Repository;
+
+public class MojioglasiViewModel extends ViewModel {
+    private Repository repository;
+    private MutableLiveData<User>mUser;
+    private MutableLiveData<List<Product>>mProduct;
 
     public MojioglasiViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is mojioglasi fragment");
+        repository=new Repository();
+        mUser=new MutableLiveData<>();
+        mProduct=new MutableLiveData<>();
     }
 
-    LiveData<String> getText() {
-        return mText;
+    //fix me
+    MutableLiveData<User>getUser(){
+        mUser=repository.getUser();
+        return  mUser;
+    }
+    MutableLiveData<List<Product>>getUserProducts(String userID){
+        mProduct=repository.getUserProducts(userID);
+        return mProduct;
     }
 }
