d181a19a933c369aacec9c32a4b1aad8d66ee26e
Olja <olivera.vorkapic@gmail.com>
2020-06-20 21:28:41 +0200

    added messages, receiverId has to be changed

--- app/src/main/java/vukan/com/apursp/adapters/MessageAdapter.java
+++ app/src/main/java/vukan/com/apursp/adapters/MessageAdapter.java
@@ -4,6 +4,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
